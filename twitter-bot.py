f5eae01e7106fc543be0e25baf40c1d01c19df69
xxCryptoxx <usansasa@gmail.com>
2021-05-21 22:32:36 +0200

    added functions

--- twitter-bot.py
+++ twitter-bot.py
@@ -1,31 +1,43 @@
 import tweepy
 # NOTE TWITTER ALLOWS 2,400 TWEETS PER DAY...sadly :/
 # Get keys from https://developer.twitter.com/en
-consumer_key = 'API KEY HERE'
-consumer_secret = 'API SECRET KEY HERE'
-access_token = 'ACCESS TOKEN HERE'
-access_token_secret = 'ACCESS TOKEN SECRET HERE'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-user = api.me()
+def secret():
+    consumer_key = 'API_KEY_HERE'
+    consumer_secret = 'API_SECRET_KEY_HERE'
+    access_token = 'ACCESS_TOKEN_HERE'
+    access_token_secret = 'ACCESS_TOKEN_SECRET_HERE'
+    
+    authorize(consumer_key, consumer_secret, access_token, access_token_secret)
 
-print('I am ' + user.name)
+def authorize(consumer_key, consumer_secret, access_token, access_token_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    
+    main(auth)
 
-#user_id=  elonmusk----geekulcha----spacex------tesla
-user_id = '44196397', '728018491', '34743251', '13298072'
-lang='en'
-numberOfTweets = 200 # Amount of tweets you want retweeted
+def main(auth):
+    api = tweepy.API(auth)
+    user = api.me()
 
-for theid in user_id:
-    for tweet in tweepy.Cursor(api.user_timeline, id=theid, lang="en").items(numberOfTweets):
-        try:
-            print('Tweet from: @' + tweet.user.screen_name)
-            tweet.favorite() # Like
-            tweet.retweet() # Retweet
-            print('You Just Retweeted A Tweet :)')
-        except tweepy.TweepError as e:
-            print(e.reason)
-        except StopIteration:
-            break
+    print('I am ' + user.name)
+
+    #user_id=  elonmusk----geekulcha----spacex------tesla
+    user_id = '44196397', '728018491', '34743251', '13298072'
+    lang='en'
+    numberOfTweets = 200 # Amount of tweets you want retweeted
+
+    for theid in user_id:
+        for tweet in tweepy.Cursor(api.user_timeline, id=theid, lang="en").items(numberOfTweets):
+            try:
+                print('Tweet from: @' + tweet.user.screen_name)
+                tweet.favorite() # Like
+                tweet.retweet() # Retweet
+                print('You Just Retweeted A Tweet :)')
+            except tweepy.TweepError as e:
+                print(e.reason)
+            except StopIteration:
+                break
+
+if __name__ == "__main__":
+    secret()
