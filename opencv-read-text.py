97c2571a4798836c540979bac3dbe123d181b220
Daryl <usansasa@gmail.com>
2021-06-26 20:38:43 +0200

    Update opencv-read-text.py

--- opencv-read-text.py
+++ opencv-read-text.py
@@ -5,7 +5,7 @@ import pytesseract
 # input -> process -> output
 
 # load image
-img = cv2.imread('bitcoin.jpeg')
+img = cv2.imread('YOUR_IMAGE_HERE.EXT')
 
 # extract text from image
 text = pytesseract.image_to_string(img)
